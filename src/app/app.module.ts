6fc3b4372fbb1dbbc2bd4e1d3e690dc7abd3eb0c
Falcon <falcondelux@gmail.com>
2021-07-21 00:34:14 +0200

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+//import { ContadorComponent } from './contador/contador/contador.component';
+import { HeroesModule } from './heroes/heroes.module';
+import { ContadorModule } from './contador/contador/contador.module';
+
+
+//Tarea:
+//Crear un módulo llamado contadorModule
+//declaraciones y exportaciones
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeroesModule,
+    ContadorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
